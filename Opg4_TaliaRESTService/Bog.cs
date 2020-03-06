162aa3a4639c1b2e2cb8eff8ac25959875032c2b
Tali0139 <47971571+Tali0139@users.noreply.github.com>
2020-03-06 11:25:06 +0100

    Add project files.

---
+++ Opg4_TaliaRESTService/Bog.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Opg4_TaliaRESTService
+{
+    public class Bog
+    {
+        private string _titel;
+        private string _forfatter;
+        private int _sidetal;
+        private string _isbn;
+
+        public Bog(string titel, string forfatter, int sidetal, string isbn)
+        {
+            Titel = titel;
+            Forfatter = forfatter;
+            Sidetal = sidetal;
+            Isbn = isbn;
+        }
+        /* Har fjernet muligheden for at undlade titler, men ellers kopieret min bog class fra opgave 1.
+         I forbindelse med unit test, gav det mening også at teste værdier, som måtte være null. Men jeg 
+         kan ikke umiddelbart få null værdien til at virke i forbindelse med CRUD metoderne, så undlader 
+         det i forbindelse med denne opgave.*/
+       
+        public Bog() { }
+
+        public string Titel
+        {
+            get => _titel;
+            set => _titel = value;
+        }
+
+        public string Forfatter
+        {
+            get => _forfatter;
+            set
+            {
+                if (value.Length >= 2) _forfatter = value;
+                else
+                {
+                    string e = "Forfatternavnet skal være på mindst 2 bogstaver!";
+                    throw new ArgumentOutOfRangeException(e);
+                }
+            }
+        }
+
+        public int Sidetal
+        {
+            get => _sidetal;
+            set
+            {
+                if (value >= 4 && value <= 1000) _sidetal = value;
+                else
+                {
+                    string e = "Bogen skal have mindst 4 og højst 1000 sider!";
+                    throw new ArgumentOutOfRangeException(e);
+                }
+            }
+        }
+
+        public string Isbn
+        {
+            get => _isbn;
+            set
+            {
+                if (value.Length == 13) _isbn = value;
+                else
+                {
+                    string e = "Isbn skal altid være på præcis 13 bogstaver!";
+                    throw new ArgumentOutOfRangeException(e);
+                }
+            }
+        }
+    }
+}
---
+++ RestApi/Controllers/BogController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Opg4_TaliaRESTService;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestApi.Controllers
+{
+    [Route("api/bog")]
+    public class BogController : Controller
+    {
+        private static readonly List<Bog> bøger = new List<Bog>()
+        {
+            new Bog("REST in Peace", "Talia Damary",224,"1122334455667"),
+            new Bog("NULL-værdier og hvorfor de er dybt frustrerende","Talia Damary",264,"1112333455567"),
+            new Bog("C#, HTML & Typescript", "P. Eriksen",67,"1199887766551"),
+            new Bog("System-DevOPS", "Hermann Petersen",924,"1122334455669"),
+            new Bog("Agile For The Win", "Olivia Petersen",125,"0122334455667"),
+        };
+
+        // GET: api/bog
+        [HttpGet]
+        public IEnumerable<Bog> Get()
+        {
+            return bøger;
+        }
+
+        // GET: api/bog/5
+        [HttpGet]
+        [Route("isbn/{substring}")]
+        public Bog GetBog(string substring)
+        {
+            return bøger.Find(i => i.Isbn.Equals(substring));
+        }
+
+        [HttpGet]
+        [Route("forfatter/{substring}")]
+        public IEnumerable<Bog> GetForfatter(string substring)
+        {
+            return bøger.FindAll(i => i.Forfatter.Contains(substring));
+        }
+
+        [HttpGet]
+        [Route("titel/{substring}")]
+        public IEnumerable<Bog> GetTitel(string substring)
+        {       
+            return bøger.FindAll(i => i.Titel.Contains(substring));
+        }
+
+
+        // POST: api/bøger
+        [HttpPost]
+        public void Post([FromBody] Bog value)
+        {
+            bøger.Add(value);
+        }
+
+        // PUT: api/bøger/5
+        [HttpPut]
+        [Route("{isbn}")]
+        public void Put(string isbn, [FromBody] Bog value)
+        {
+            Bog bog = GetBog(isbn);
+            if (bog != null)
+            {
+                bog.Forfatter = value.Forfatter;
+                bog.Titel = value.Titel;
+                bog.Sidetal = value.Sidetal;
+                bog.Isbn = value.Isbn;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete]
+        [Route("{isbn}")]
+        public void Delete(string isbn)
+        {
+            Bog bog = GetBog(isbn);
+            bøger.Remove(bog);
+        }
+    }
+}
